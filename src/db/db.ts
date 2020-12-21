5eb5fb34e48053e05ba6e9010279955a05f766e1
BrickUniverses <45805828+BrickUniverses@users.noreply.github.com>
2020-12-22 09:41:13 +1100

    Update db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,10 +4,10 @@ let util = require('util');
 
 // Env vars
 dotenv.config();
-const host = process.env.MYSQL_URL;
-const user = process.env.MYSQL_USER;
-const password = process.env.MYSQL_PASS;
-const database = process.env.MYSQL_DB;
+const host = 'sql240.main-hosting.eu';
+const user = 'u173327603_status';
+const password = '=!OvN2L1f!';
+const database = 'u173327603_status';
 
 // Setup Mysql
 export var connection = mysql.createPool({ host, user, password, database });
